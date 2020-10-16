db808ddfab8ecfbe3e58ac9b1788dba7b21d0040
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-10-16 13:39:57 +0900

    feat: HTTPリクエストの処理時間をメトリクスとして収集するハンドラを追加 (#5)
    * feat: HTTPリクエストの処理時間をメトリクスとして収集するハンドラを追加
    
    * fix: レビュー指摘反映、メソッド引数の型の取得をgetParameterTypes()を使った形に修正
    
    * fix: レビュー指摘反映、変数名の誤記修正

---
+++ src/main/java/nablarch/integration/micrometer/instrument/handler/http/DefaultHttpRequestMetricsTagBuilder.java
@@ -0,0 +1,125 @@
+package nablarch.integration.micrometer.instrument.handler.http;
+
+import io.micrometer.core.instrument.Tag;
+import nablarch.fw.ExecutionContext;
+import nablarch.fw.handler.MethodBinding;
+import nablarch.fw.web.HttpRequest;
+import nablarch.fw.web.servlet.ServletExecutionContext;
+
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * {@link HttpRequestMetricsTagBuilder}のデフォルト実装。
+ * <p>
+ * タグのリストは以下の内容で作成する。
+ * <table>
+ *   <tr>
+ *     <th>タグ</th>
+ *     <th>説明</th>
+ *   </tr>
+ *   <tr>
+ *     <td>class</td>
+ *     <td>
+ *       リクエストを処理したクラスの名前({@link Class#getName()})。<br>
+ *       クラスの情報を取得できない場合は {@code UNKNOWN}。
+ *     </td>
+ *   </tr>
+ *   <tr>
+ *     <td>method</td>
+ *     <td>
+ *       リクエストを処理したメソッドを表す文字列。<br>
+ *       この文字列は、メソッド名の後ろに引数の型の正規名({@link Class#getCanonicalName()})をアンダースコア({@code _})で
+ *       つなげたものになる（例:{@code fooMethod_int_java.lang.String}）。<br>
+ *       メソッドの情報を取得できない場合は {@code UNKNOWN}。
+ *     </td>
+ *   </tr>
+ *   <tr>
+ *     <td>httpMethod</td>
+ *     <td>HTTPメソッド</td>
+ *   </tr>
+ *   <tr>
+ *     <td>status</td>
+ *     <td>HTTPステータスコード</td>
+ *   </tr>
+ *   <tr>
+ *     <td>outcome</td>
+ *     <td>
+ *       HTTPステータスコードの種類を表す文字列。<br>
+ *       1XX は {@code INFORMATION}, 2XX は {@code SUCCESS}, 3XX は {@code REDIRECTION},
+ *       4XX は {@code CLIENT_ERROR}, 5XX は {@code SERVER_ERROR}, それ以外の場合は {@code UNKNOWN}。
+ *     </td>
+ *   </tr>
+ *   <tr>
+ *     <td>exception</td>
+ *     <td>例外がスローされた場合は、そのクラスの単純名（スローされていない場合は {@code "None"}）</td>
+ *   </tr>
+ * </table>
+ * </p>
+ */
+public class DefaultHttpRequestMetricsTagBuilder implements HttpRequestMetricsTagBuilder {
+
+    @Override
+    public List<Tag> build(HttpRequest request, ExecutionContext context, Throwable thrownThrowable) {
+        Class<?> clazz = context.getRequestScopedVar(MethodBinding.SCOPE_VAR_NAME_BOUND_CLASS);
+        Method method = context.getRequestScopedVar(MethodBinding.SCOPE_VAR_NAME_BOUND_METHOD);
+        HttpServletResponse servletResponse = ((ServletExecutionContext) context).getServletResponse();
+
+        /*
+         * タグ仕様の背景説明
+         *
+         * ここで設定しているタグは、 Spring Boot Actuator の Spring MVC Metrics が設定しているタグを参考にしている。
+         * ただし、 Actuator は uri タグを設定しているのに対して、本メトリクスでは class, method を設定している違いがある。
+         * Actuator の uri は、リクエストを受けたパスの定義(/project/{id})が設定される。
+         * しかし、このパス定義に該当する情報は、 Nablarch が利用している HTTP Request Router から取得できない。
+         * したがって、本メトリクスでは uri の代わりに実行されたアクションクラスとメソッドの情報を出力している。
+         * 参考: https://spring.pleiades.io/spring-boot/docs/2.3.4.RELEASE/reference/html/production-ready-features.html#production-ready-metrics-spring-mvc
+         *
+         * class, method タグの出力書式については、 Eclipse Microprofile の Metrics の仕様を参考にしている。
+         * また、 class が Class#getName() で method の引数の型が Class#getCanonicalName() である点については、
+         * Microprofile の Metrics を実装している Open Liberty の実際の動作を参考にしている。
+         * 参考: https://download.eclipse.org/microprofile/microprofile-metrics-2.3/microprofile-metrics-spec-2.3.html#_optional_rest
+         */
+        return Arrays.asList(
+            Tag.of("class", clazz != null ? clazz.getName() : "UNKNOWN"),
+            Tag.of("method", method != null ? buildMethodTag(method) : "UNKNOWN"),
+            Tag.of("httpMethod", request.getMethod()),
+            Tag.of("status", String.valueOf(servletResponse.getStatus())),
+            Tag.of("outcome", resolveOutcome(servletResponse.getStatus())),
+            Tag.of("exception", resolveException(context, thrownThrowable))
+        );
+    }
+
+    private String buildMethodTag(Method method) {
+        StringBuilder sb = new StringBuilder(method.getName());
+
+        for (Class<?> parameterType : method.getParameterTypes()) {
+            sb.append("_").append(parameterType.getCanonicalName());
+        }
+
+        return sb.toString();
+    }
+
+    private String resolveOutcome(int statusCode) {
+        if (100 <= statusCode && statusCode < 200) {
+            return "INFORMATION";
+        } else if (200 <= statusCode && statusCode < 300) {
+            return "SUCCESS";
+        } else if (300 <= statusCode && statusCode < 400) {
+            return "REDIRECTION";
+        } else if (400 <= statusCode && statusCode < 500) {
+            return "CLIENT_ERROR";
+        } else if (500 <= statusCode && statusCode < 600) {
+            return "SERVER_ERROR";
+        } else {
+            return "UNKNOWN";
+        }
+    }
+
+    private String resolveException(ExecutionContext context, Throwable thrownThrowable) {
+        Throwable throwable = thrownThrowable != null ? thrownThrowable : context.getException();
+        return throwable == null ? "None" : throwable.getClass().getSimpleName();
+    }
+}
---
+++ src/test/java/nablarch/integration/micrometer/instrument/handler/http/HttpRequestMetricsHandlerTest.java
@@ -0,0 +1,98 @@
+package nablarch.integration.micrometer.instrument.handler.http;
+
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import mockit.Expectations;
+import mockit.Mocked;
+import nablarch.fw.web.HttpRequest;
+import nablarch.fw.web.HttpResponse;
+import nablarch.fw.web.servlet.ServletExecutionContext;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThrows;
+
+/**
+ * {@link HttpRequestMetricsHandler}の単体テスト。
+ * @author Tanaka Tomoyuki
+ */
+public class HttpRequestMetricsHandlerTest {
+    @Mocked
+    private HttpRequest request;
+    @Mocked
+    private HttpResponse response;
+    @Mocked
+    private ServletExecutionContext context;
+    @Mocked
+    private HttpRequestMetricsTagBuilder tagBuilder;
+
+    private HttpRequestMetricsHandler sut;
+    private SimpleMeterRegistry registry;
+
+    @Before
+    public void setUp() {
+        sut = new HttpRequestMetricsHandler();
+        sut.setHttpRequestMetricsTagBuilder(tagBuilder);
+        registry = new SimpleMeterRegistry();
+        sut.setMeterRegistry(registry);
+    }
+
+    @Test
+    public void testInvokeNextHandler() {
+        new Expectations() {{
+            context.handleNext(request); result = response;
+        }};
+
+        HttpResponse response = sut.handle(request, context);
+
+        assertThat(response, is(sameInstance(response)));
+    }
+
+    @Test
+    public void testMeasureTime() {
+        new Expectations() {{
+            context.handleNext(request); result = response;
+
+            tagBuilder.build(request, context, null);
+            result = Arrays.asList(Tag.of("foo", "FOO"), Tag.of("bar", "BAR"));
+        }};
+
+        sut.handle(request, context);
+
+        Timer timer = registry.get("http.server.requests").timer();
+        assertThat(timer.count(), is(1L));
+
+        Meter.Id id = timer.getId();
+        assertThat(id.getTag("foo"), is("FOO"));
+        assertThat(id.getTag("bar"), is("BAR"));
+        assertThat(id.getDescription(), is("HTTP request metrics measured by Timer."));
+    }
+
+    @Test
+    public void testTimerThrowsExceptionCase() {
+        new Expectations() {{
+            context.handleNext(request); result = new Throwable("test throwable");
+        }};
+
+        Throwable throwable = assertThrows(Throwable.class, () -> sut.handle(request, context));
+        assertThat(throwable.getMessage(), is("test throwable"));
+
+        Timer timer = registry.get("http.server.requests").timer();
+        assertThat(timer.count(), is(1L));
+    }
+
+    @Test
+    public void testThrowsExceptionIfMeterRegistryIsNull() {
+        sut.setMeterRegistry(null);
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> sut.handle(request, context));
+        assertThat(exception.getMessage(), is("meterRegistry is not set."));
+    }
+}
