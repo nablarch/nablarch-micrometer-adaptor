9fb7a9bd6ba060a8dc16019fe686c5bbcc075082
junya koyama <arukiidou@yahoo.co.jp>
2024-05-30 06:45:44 +0900

    NAB-554 バージョンアップに伴うテスト対象プロパティ追加
    Signed-off-by: junya koyama <arukiidou@yahoo.co.jp>

--- src/test/java/nablarch/integration/micrometer/otlp/OtlpMeterRegistryFactoryTest.java
+++ src/test/java/nablarch/integration/micrometer/otlp/OtlpMeterRegistryFactoryTest.java
@@ -1,5 +1,8 @@
 package nablarch.integration.micrometer.otlp;
 
+import io.micrometer.core.instrument.config.validate.Validated;
+import io.micrometer.core.instrument.config.validate.ValidationException;
+import io.micrometer.registry.otlp.AggregationTemporality;
 import io.micrometer.registry.otlp.OtlpMeterRegistry;
 import mockit.Deencapsulation;
 import nablarch.core.repository.disposal.BasicApplicationDisposer;
@@ -8,9 +11,13 @@ import org.junit.Test;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThrows;
 
 /**
  * {@link OtlpMeterRegistryFactory}の単体テスト。
@@ -37,15 +44,39 @@ public class OtlpMeterRegistryFactoryTest {
         // setPrefixを行うと、subPrefixとは関係なく指定のprefixを取得するようになる
         assertThat(config.prefix(), is("test.otlp"));
         // From OtlpConfig
-        assertThat(config.url(), is("http://localhost:9090/api/v1/otlp/v1/metrics"));
+        assertThat(config.aggregationTemporality(), is(AggregationTemporality.DELTA));
+        assertThat(config.baseTimeUnit(), is(TimeUnit.SECONDS));
+        assertThat(config.headers(), hasEntry("Api-Key", "NRIA****"));
         assertThat(config.resourceAttributes(), allOf(
                 hasEntry("service.name", "nablarch-test"),
                 hasEntry("url.scheme", "http"),
                 hasEntry("service.version", "v1alpha1")
         ));
+        assertThat(config.url(), is("http://localhost:9090/api/v1/otlp/v1/metrics"));
 
         assertThat(config.validate().failures(), empty());
         assertThat(config.validate().isValid(), is(true));
+    }
+
+    /**
+     * 空ではない、精査エラーとなるpropertiesファイルを設定した時のテストケース。
+     * @author Junya Koyama
+     */
+    @Test
+    public void testInvalid() {
+        OtlpMeterRegistryFactory sut = new OtlpMeterRegistryFactory();
+        sut.setApplicationDisposer(new BasicApplicationDisposer());
+        sut.setMeterBinderListProvider(new DefaultMeterBinderListProvider());
+        sut.setPrefix("test.otlp");
+        sut.setXmlConfigPath("nablarch/integration/micrometer/otlp/OtlpMeterRegistryFactory/testCreateObject/test.invalid.xml");
+
+        ValidationException thrown = assertThrows(ValidationException.class, sut::createObject);
+        List<String> errors = thrown.getValidation().failures()
+                .stream().map(Validated.Invalid::getMessage).collect(Collectors.toList());
+        assertThat(errors, contains(
+                "must contain a valid time unit",
+                "should be one of 'DELTA', 'CUMULATIVE'"
+        ));
     }
 
     /**
@@ -71,8 +102,11 @@ public class OtlpMeterRegistryFactoryTest {
                 is("nablarch.micrometer.otlp")
         ));
         // From OtlpConfig
-        assertThat(config.url(), is("http://localhost:4318/v1/metrics"));
+        assertThat(config.aggregationTemporality(), is(AggregationTemporality.CUMULATIVE));
+        assertThat(config.baseTimeUnit(), is(TimeUnit.MILLISECONDS));
+        assertThat(config.headers(), is(Collections.<String, String>emptyMap()));
         assertThat(config.resourceAttributes(), is(Collections.<String, String>emptyMap()));
+        assertThat(config.url(), is("http://localhost:4318/v1/metrics"));
         // From PushRegistryConfig
         assertThat(config.step(), is(Duration.ofMinutes(1L)));
         assertThat(config.enabled(), is(true));
